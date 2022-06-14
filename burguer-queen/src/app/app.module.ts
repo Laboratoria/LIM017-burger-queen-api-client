b3502d780fa5b33e37d60bee6c127fcf29f77561
LGomez96 <lindagomez96@gmail.com>
2022-06-14 12:22:25 -0500

    hoja js con data simulada

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponentComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
