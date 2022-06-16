ca76842a598f06506364aec486d9520af6bf79a2
Angelica Bolivar <angelica.bolivarvc@gmail.com>
2022-06-15 20:00:55 -0500

    onboarding-login

--- src/components/Onboarding.jsx
+++ src/components/Onboarding.jsx
@@ -10,11 +10,12 @@ export function Onboarding(){
   // function RouterLogin(){
   //   history.push('/login')
   // };
+  const navigate = useNavigate()
   return (
     <div className="App">
         <h3 className="title-login">Burger Queen</h3>
         <img src="./src/image/Burger.png" className="img-fluid img" alt="Burger Queen"></img>
-        <button onClick={Navigate} type="submit" className="btn btn-primary">Ingresar</button>
+        <button onClick={() => navigate('/login')} type="submit" className="btn btn-primary">Ingresar</button>
 
     </div>
   )
