b52168150c5dd7188d8346802855f352bb152802
LGomez96 <lindagomez96@gmail.com>
2022-06-20 17:45:10 -0500

    registro exitoso

--- burguer-queen/src/app/register/register.component.ts
+++ burguer-queen/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MenuService } from '../services/menu.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,13 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form! : FormGroup;
+  messageError: undefined;
+  messageInfo!: undefined;
+  constructor(public formbuilder: FormBuilder, private menuService: MenuService, private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    //se inicializan los input que vamos a capturar:
+    //1.- llamar al FormGroup y le asignamos un grupo del constructor
+    this.form = this.formbuilder.group(
+      //los hijos de control:
+      {
+        email: [''],
+        password: [''],
+      }
+    )
   }
-  onSubmit(values: any): void{
-    console.info(values)
+  onSubmitInRegister(){
+    this.menuService.addUsers('http://localhost:3001/auth',
+    {
+      email: this.form.value.email,
+      password: this.form.value.password
+    })
+    .subscribe(
+      res => { 
+         setTimeout(() => {
+          this.router.navigate(['/login'])
+        }, 2000)
+        
+
+        console.info(res)
+      },
+      error => {
+        this.messageError = error.status;
+      }
+      
+    )
+    //es como un evento y vamos a capturar valores y asignarlos a:
+    
   }
 
 }
