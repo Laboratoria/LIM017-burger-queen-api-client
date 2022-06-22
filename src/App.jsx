69204f1150e3a7dab21cb7d0e4ba98e753986232
Angelica Bolivar <angelica.bolivarvc@gmail.com>
2022-06-21 22:06:48 -0500

    navbar-component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import {Login} from './components/Login'
 import {Onboarding} from './components/Onboarding'
+import {Navbar} from '../src/components/Navbar/Navbar'
 
 function App() {
  return (
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Onboarding/>}/>
-      <Route path='/login' element={<Login/>}/>
+      {/* <Route path='/' element={<Onboarding/>}/>
+      <Route path='/login' element={<Login/>}/> */}
+      <Route path='/Navbar' element={<Navbar/>}/>
     </Routes>
   </BrowserRouter>
  )
