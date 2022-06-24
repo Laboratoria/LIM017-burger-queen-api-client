762d182009977186d77dbe36ede0b92c40ca83d6
madelen <madelen2811@gmail.com>
2022-06-24 15:12:55 -0500

    cambios admin

--- burguer-queen/src/app/admin/admin.component.ts
+++ burguer-queen/src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Order } from '../app.module';
 import { MenuService } from '../services/menu.service';
 
@@ -9,12 +9,15 @@ import { MenuService } from '../services/menu.service';
 })
 export class AdminComponent implements OnInit {
   menu: Order[] = [];
-
+@Output() productDeleteEvent = new EventEmitter<string>();
 
   constructor(private menuService: MenuService) { }
 
   ngOnInit(): void {
     this.menuService.getOrder().subscribe((menu) => (this.menu = menu, console.log(menu)));
   }
-
+ 
+  onProductDelete(id: string):void{
+    this.productDeleteEvent.emit(id);
+  }
 }
