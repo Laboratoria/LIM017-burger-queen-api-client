8741ef95144a97f3cf5a9857364593ad9915078d
LissetteMH <lissette.machaca@gmail.com>
2022-06-25 19:36:14 -0500

    cambios de  login usando react hook form

--- burgerqueen-react/src/views/Inicio/Login.jsx
+++ burgerqueen-react/src/views/Inicio/Login.jsx
@@ -1,62 +1,69 @@
 import './Login.css';
-import {useState} from 'react';
+// import { useState } from 'react';
 import logo from '../newDir/IconBurger.svg';
 import { useNavigate } from "react-router-dom"
+import { useForm } from 'react-hook-form'
+
+// Petición HTTP
+const request = ({email, password}, url) => new Promise ((resolve, reject) =>{
+  return  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({email, password}),
+    headers: {
+      'Content-Type': 'application/json' 
+    }
+  })
+  .then(res => res.json())
+  .then(response => resolve(response.token))
+  .catch(error => reject(error))
+})
 
 // COMPONENTE PARA FORMULARIO
 
 const LoginForm = () => {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
-    const [datos, setDatos] = useState({
-        email: '',
-        password: '',
-    })
+  const {register, formState: { errors }, handleSubmit} = useForm();
 
-    const handleInputChange = (event) => {
-        // console.log(event.target.name)
-        // console.log(event.target.value)
-        setDatos({
-            ...datos,
-            [event.target.name] : event.target.value
-        })
-    }
-
-    const enviarDatos = (event) => {
-        event.preventDefault()
-        console.log(`enviando datos nuevos: ${datos.email}, ${datos.password}`);
-        // 
+  const onSubmit = (data, event) => {
         const url = 'http://localhost:3001/auth';
-        const {email, password} = datos;
-        console.log({email, password})
-
-        fetch(url, {
-          method: 'POST', // or 'PUT'
-          body: JSON.stringify({email, password}),
-          headers: {
-            'Content-Type': 'application/json' 
-          }
-        }).then(res => res.json())
-        .then(response => {
-          if(response.token !== undefined){
-            console.log('tenemos token', response)
-
+        const {email, password} = data;
+        console.log({email, password});
+        request({email, password}, url)
+        .then((res) => {
+          if (res !== undefined){
             return navigate('/Waiter')
-            
-          } return console.log('no hay token , es 400');
-        })
-        .catch(error => console.error('Error:', error));
-        // 
-    }
+          } else{
+            return console.log("Error 400", res)
+          }
 
+        })
+        .catch((error) => console.log(error.message))      
+
+        event.target.reset();
+  }
     return (
-            <form className="loginForm" onSubmit={enviarDatos}>
+            <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
                 <div className="col-md-3">
                   <label className='label-form'>
                     Email:
                     <div className='div__Icon-Input'>
-                    <i className="fa-solid fa-envelope Icon"></i>
-                    <input type="email" placeholder="example@example.com" className="input-form-control" onChange={handleInputChange} name="email"></input>
+                      <i className="fa-solid fa-envelope Icon"></i>
+                      <input 
+                        type="email" 
+                        placeholder="example@example.com" 
+                        className="input-form-control" 
+                        name="email"
+                        {...register('email',{
+                          required: {
+                              value: true, 
+                              message: 'Email is required'
+                              } 
+                      })}
+                      />
+                      <span className="text-danger text-small d-block mb-2">
+                        {errors?.email?.message}
+                      </span>
                     </div>
                   </label>
                 </div>
@@ -64,8 +71,30 @@ const LoginForm = () => {
                   <label className='label-form'>
                     Password:
                     <div className='div__Icon-Input'>
-                    <i className="fa-solid fa-lock Icon"></i>
-                    <input type="password" placeholder="******" className="input-form-control" onChange={handleInputChange} name="password"></input>
+                      <i className="fa-solid fa-lock Icon"></i>
+                      <input 
+                        type="password" 
+                        placeholder="******" 
+                        className="input-form-control" 
+                        name="password"
+                        {...register('password',{
+                          required: {
+                              value: true, 
+                              message: 'Password is required'
+                              }, 
+                          maxLength: {
+                              value: 30, 
+                              message: 'No more than 30 characteres!'
+                              },
+                          minLength: {
+                              value: 6, 
+                              message: 'At least 6 characteres!'
+                              }
+                      })}
+                      />
+                      <span className="text-danger text-small d-block mb-2">
+                        {errors?.password?.message}
+                      </span>
                     </div>
                   </label>
                 </div>
