f9c25d7d257eb2e935e9cd2d3b702c9dd674646a
Emilce Toledo <toledo.emilce.a@gmail.com>
2022-07-07 09:33:15 -0500

    Cambios

--- src/services/orders.js
+++ src/services/orders.js
@@ -2,21 +2,22 @@ import axios from 'axios';
 // import {urlBase} from '../config'
 
 // const urlOrders = `${urlBase}/orders`
+let url="http://localhost:5000/orders";
 
 export const createOrder = (url, data)=>{
   return axios.post(url, data)
 }
 
 export const getOrders = () => {
-  return axios.get(urlOrders)
+  return axios.get(url)
 }
 
 export const getOrder = (id) => {
-  const urlOrder = `${urlOrders}/${id}`
+  const urlOrder = `${url}/${id}`
   return axios.get(urlOrder)
 }
 
 export const putOrder =(id, data) =>{
-  const urlOrder = `${urlOrders}/${id}`
+  const urlOrder = `${url}/${id}`
   return axios.put(urlOrder, data)
 }
